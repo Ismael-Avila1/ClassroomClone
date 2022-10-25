0a7b7ac0aaeb59f87cee048402af7719a11bb9a5
Ismael Avila <ismael.guada234015@gmail.com>
2022-10-25 14:28:27 -0500

    En el userIndex se agregó un enlace al nombre del usuario para que vaya directamente al userShow

--- resources/views/users/userIndex.blade.php
+++ resources/views/users/userIndex.blade.php
@@ -18,7 +18,11 @@
         @foreach($users as $user)
             <tr>
                 <td>{{ $user->id }}</td>
-                <td>{{ $user->name }}</td>
+                <td>
+                    <a href="/user/{{ $user->id }}">
+                        {{ $user->name }}
+                    </a>
+                </td>
                 <td>{{ $user->email }}</td>
             </tr>
         @endforeach
--- resources/views/users/userShow.blade.php
+++ resources/views/users/userShow.blade.php
@@ -11,5 +11,9 @@
 
     <h2>{{ $user->name }}</h2>
 
+    <p>
+        {{ $user->email }}
+    </p>
+
 </body>
 </html>
