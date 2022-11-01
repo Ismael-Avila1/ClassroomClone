a4e15471fecdce8931d8aa574b115375872cb1f7
Ismael Avila <ismael.guada234015@gmail.com>
2022-11-01 00:27:40 -0600

    Se agregó JetStream para la autenticación de usuarios.

--- resources/views/users/userIndex.blade.php
+++ resources/views/users/userIndex.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Listado de Usuarios</title>
+
+    @vite(['resources/css/materialize.css', 'resources/js/materialize.js'])
 </head>
 <body>
     <h1>Listado de Usuario</h1>
