bd1d136337e12d8a685e653d5ed4ae3998803cad
Ismael Avila <ismael.guada234015@gmail.com>
2022-11-08 12:59:38 -0600

    Se modificó la ruta raíz del proyecto. Ahora apunta a la LandingPage. Posteriormente se van a agregar los enlaces de login y registro

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingpage');
 });
 
 Route::resource('user', UserController::class);
