a54e73aec928f90d0d955d78f284942a05474238
Ismael Avila <ismael.guada234015@gmail.com>
2022-11-09 00:37:44 -0600

    Solucionado el problema con el registro de contactos. El problema era el nombre de la migración: se cambió de 'contactos' a 'contacts'

--- database/migrations/2022_11_09_062711_create_contacts_table.php
+++ database/migrations/2022_11_09_062711_create_contacts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('contactos', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contactos');
+        Schema::dropIfExists('contacts');
     }
 };
