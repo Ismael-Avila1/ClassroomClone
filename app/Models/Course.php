b1dde9bcac8f3576567c5b2cf69f729bee9b0c9e
Ismael Avila <ismael.guada234015@gmail.com>
2022-11-29 23:46:55 -0600

    Se agregó la relación Course-User, esta relación es de estudiante (usuario) a course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -20,4 +20,9 @@ class Course extends Model
     {
         return $this->hasOne(User::class);
     }
+
+    public function students()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
