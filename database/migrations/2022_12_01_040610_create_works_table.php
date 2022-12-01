8eec386f04de3993039467d3328e582bd376bae4
Ismael Avila <ismael.guada234015@gmail.com>
2022-11-30 23:54:47 -0600

    Creación de modelo y controlador de la clase Work.

--- database/migrations/2022_12_01_040610_create_works_table.php
+++ database/migrations/2022_12_01_040610_create_works_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('work', function (Blueprint $table) {
+        Schema::create('works', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('work');
+        Schema::dropIfExists('works');
     }
 };
