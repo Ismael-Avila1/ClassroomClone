57347ff240edcac05e8436a714e36a818b6b8f75
Ismael Avila <ismael.guada234015@gmail.com>
2022-12-03 16:01:09 -0600

    Implementación de barra de navegación en el componente 'template'. Esta navbar estará presente en las vistas de los cursos

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -10,6 +10,50 @@
     <link rel="stylesheet" href="/bulma-rtl.min.css">
 </head>
 <body>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+                <img src="/ClassRoomClone-Logo.png" alt="logo">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="">Trabajos</a>
+                <a class="navbar-item" href="">Personas</a>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">Administrar Cuenta</a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item button is-white">{{ Auth::user()->name }}</a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item button is-info is-fullwidth" href="user/{{ Auth::user()->id }}">Perfil</a>
+
+                        <hr class="navbar-divider">
+
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <button type="submit" class="button is-danger is-fullwidth">
+                                {{ __('Log Out') }}
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
     <h1 class="title is-size-1-desktop has-text-centered">{{ $title }}</h1>
 
     {{ $slot }}
